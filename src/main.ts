5f1a7509949ef4faaf064c27a4e32c51cd185bd9
Yura <yu.a.zolotarev@yandex.ru>
2025-10-13 14:14:04 +0300

    install mdi-icons

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,18 @@ import App from './app/App.vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 const app = createApp(App)
--- src/pages/accounts/ui/AccountPage.vue
+++ src/pages/accounts/ui/AccountPage.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { TitleComponent } from '@/shared/ui/title';
+import { mdiHelpCircleOutline } from '@mdi/js'
 </script>
 
 <template>
-  <TitleComponent title="Учетные записи" />
+  <TitleComponent title="Учетные записи" class="mb-12" />
+  <v-alert color="grey" density="compact" :icon="mdiHelpCircleOutline" class="mb-12">
+    Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;
+  </v-alert>
 </template>
 
 <style scoped></style>
