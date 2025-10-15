86e5af2a0d7a313f6fe350b9cf6b6f49d0662dd2
Yura <yu.a.zolotarev@yandex.ru>
2025-10-15 03:42:28 +0300

    add confirm

---
+++ src/shared/ui/modal/ModalDefault.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+defineProps({
+  show: Boolean
+})
+</script>
+
+<template>
+  <Transition name="modal">
+    <div v-if="show" class="modal-mask">
+      <div class="modal__container">
+        <div class="modal__header">
+          <slot name="header">default header</slot>
+        </div>
+
+        <div class="modal__body">
+          <slot name="body">default body</slot>
+        </div>
+
+        <div class="modal__footer">
+          <slot name="footer">default footer</slot>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  transition: opacity 0.3s ease;
+}
+
+.modal__container {
+  width: auto;
+  margin: auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+
+.modal__body {
+  margin: 20px 0;
+}
+
+.modal-enter-from {
+  opacity: 0;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
